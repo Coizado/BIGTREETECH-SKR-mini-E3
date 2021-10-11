65e6bf1d4eef52fae890211e9c4684c8b2a7b0de
Coizado <86024195+Coizado@users.noreply.github.com>
2021-10-11 19:31:21 -0300

    PID defaults

--- firmware/V1.2/Marlin-2.0.7.2-SKR-mini-E3-V1.2/Marlin/Configuration.h
+++ firmware/V1.2/Marlin-2.0.7.2-SKR-mini-E3-V1.2/Marlin/Configuration.h
@@ -496,9 +496,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp  19.62
+    #define DEFAULT_Ki   1.45
+    #define DEFAULT_Kd  66.46
   #endif
 #endif // PIDTEMP
 
