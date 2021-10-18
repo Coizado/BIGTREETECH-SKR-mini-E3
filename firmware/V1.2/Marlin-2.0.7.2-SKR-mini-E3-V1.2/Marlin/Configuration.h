d07726f8523072ace18b880b83b09cb6a867451b
Coizado <86024195+Coizado@users.noreply.github.com>
2021-10-18 18:22:20 -0300

    Homing speeds and probes
    Homing speeds, probe twice, second time slower, probe as close as possible to homing movement and speed for closer result between Z home and Probe on the same spot.

--- firmware/V1.2/Marlin-2.0.7.2-SKR-mini-E3-V1.2/Marlin/Configuration.h
+++ firmware/V1.2/Marlin-2.0.7.2-SKR-mini-E3-V1.2/Marlin/Configuration.h
@@ -988,13 +988,13 @@
 #define PROBING_MARGIN 10
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_SPEED (133*60)
+#define XY_PROBE_SPEED (133*40)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
 
 // Feedrate (mm/min) for the "accurate" probe of each point
-#define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 2)
+#define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 4)
 
 /**
  * Multiple Probing
@@ -1005,7 +1005,7 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-#define MULTIPLE_PROBING 3
+#define MULTIPLE_PROBING 2
 //#define EXTRA_PROBING    1
 
 /**
@@ -1388,7 +1388,7 @@
 #endif
 
 // Homing speeds (mm/min)
-#define HOMING_FEEDRATE_XY (20*60)
+#define HOMING_FEEDRATE_XY (40*60)
 #define HOMING_FEEDRATE_Z  (4*60)
 
 // Validate that endstops are triggered on homing moves
